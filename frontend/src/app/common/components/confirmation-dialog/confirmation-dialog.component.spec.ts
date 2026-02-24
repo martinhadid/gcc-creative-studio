3605afac61ef7afe7cf1f336e7db97326765f102
pradhyumnap-hub <pradhyumnap@google.com>
2026-02-24 09:06:09 +0000

    login module test cases with fixes

--- frontend/src/app/common/components/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ frontend/src/app/common/components/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-
-import {ConfirmationDialogComponent} from './confirmation-dialog.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from './confirmation-dialog.component';
 
 describe('ConfirmationDialogComponent', () => {
   let component: ConfirmationDialogComponent;
@@ -25,6 +25,11 @@ describe('ConfirmationDialogComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ConfirmationDialogComponent],
+      imports: [MatDialogModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ConfirmationDialogComponent);
