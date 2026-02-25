a92f452bfc9b92bacbdabd96c417c95f29225195
pradhyumnap-hub <pradhyumnap@google.com>
2026-02-25 12:13:33 +0000

    undoing changes  as it is develop

--- frontend/src/app/app-injector.ts
+++ frontend/src/app/app-injector.ts
@@ -19,5 +19,9 @@ import { Injector } from '@angular/core';
 export let AppInjector: Injector;
 
 export function setAppInjector(injector: Injector) {
-  AppInjector = injector;
+  if (AppInjector) {
+    console.error('AppInjector is already set');
+  } else {
+    AppInjector = injector;
+  }
 }
